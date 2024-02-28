add8aff2329ac9cd8bef01268a0c5034f75895c8
AniketPawase <138360404+AniketPawase@users.noreply.github.com>
2024-02-28 20:58:09 +0530

    minor changes10

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((request,response,next)=>{
     response.setHeader('Access-Control-Allow-Origin',"*");
     response.setHeader('Access-Control-Allow-Headers',"*");
     response.setHeader('Access-Control-Allow-Methods',"*");
-    res.header('Access-Control-Allow-Credentials', 'true');
+    response.header('Access-Control-Allow-Credentials', 'true');
     next();
 })
 
